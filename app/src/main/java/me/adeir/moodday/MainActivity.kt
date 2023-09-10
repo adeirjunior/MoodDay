2320f1b7bdc7b754756c6cf7e07fc440a9397400
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2023-09-10 19:56:23 -0400

    navigation implemented

--- app/src/main/java/me/adeir/moodday/MainActivity.kt
+++ app/src/main/java/me/adeir/moodday/MainActivity.kt
@@ -3,13 +3,19 @@ package me.adeir.moodday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import me.adeir.moodday.ui.theme.MoodDayTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +23,70 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MoodDayTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Adeir")
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = Route.screenOne
+                    ) {
+                        composable(route = Route.screenOne) {
+                            ScreenOne (
+                                navigateToScreenTwo = {
+                                    navController.navigate(Route.screenTwo)
+                                }
+                            )
+                        }
+                        composable(route = Route.screenTwo) {
+                            ScreenTwo (
+                                navigateToScreenOne = {
+                                    navController.navigate(Route.screenOne)
+                                }
+                            )
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+object Route {
+    const val screenOne = "screenOne"
+    const val screenTwo = "screenTwo"
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MoodDayTheme {
-        Greeting("Android")
+fun ScreenOne(
+    navigateToScreenTwo: () -> Unit
+) {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Screen 1")
+        Button(onClick = navigateToScreenTwo) {
+            Text("Navigate to Screen 2")
+        }
+    }
+}
+
+@Composable
+fun ScreenTwo(
+    navigateToScreenOne: () -> Unit
+) {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Screen 2")
+        Button(onClick = navigateToScreenOne) {
+            Text("Navigate to Screen 1")
+        }
     }
 }
