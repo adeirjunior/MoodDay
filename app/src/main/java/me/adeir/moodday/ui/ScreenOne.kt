80f2dd477022f3ce73cd613802e2cb483994905c
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2023-10-24 23:01:07 -0400

    lazy column test

--- app/src/main/java/me/adeir/moodday/ui/ScreenOne.kt
+++ app/src/main/java/me/adeir/moodday/ui/ScreenOne.kt
@@ -8,10 +8,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 
+val itemsList = (0..50).toList()
+val itemsIndexedList = listOf("A", "B", "C")
+
+class ScreenOneProvider : PreviewParameterProvider<() -> Unit> {
+    override val values: Sequence<() -> Unit> = sequenceOf({})
+}
+
+
+@Preview()
 @Composable
 fun ScreenOne(
-    navigateToScreenTwo: () -> Unit
+    @PreviewParameter(ScreenOneProvider::class) navigateToScreenTwo: () -> Unit
 ) {
     Column (
         modifier = Modifier.fillMaxSize(),
@@ -21,6 +36,20 @@ fun ScreenOne(
         Text("Home Page")
         Button(onClick = navigateToScreenTwo) {
             Text("Navigate to Screen 2")
+        }
+    }
+
+    LazyColumn {
+        items(itemsList) {
+            Text("Item is $it")
+        }
+
+        item {
+            Text("Single item")
+        }
+
+        itemsIndexed(itemsIndexedList) { index, item ->
+            Text("Item at index $index is $item")
         }
     }
 }
